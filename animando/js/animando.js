02b6f8978d5b1d6a34d53beb53fb15c4de05333f
mrndelfino <39888546+mrndelfino@users.noreply.github.com>
2018-11-03 23:12:40 -0300

    Silent fix

--- animando/js/animando.js
+++ animando/js/animando.js
@@ -1,20 +1,16 @@
-<script type="text/javascript">
-    
-    $(document).ready(function() 
-    {
-        $("body").css("display", "none")}
-    
-        $("body").fadeIn(2000);
-    
-        $("a").click(function(event)
-        {
-            event.preventDefault();
-            linkLocation = this.href;
-            $("body").fadeOut(1000, redirectPage);      
-        });
-        function redirectPage() 
-        {
-            window.location = linkLocation
-        }              
-    });
-</script>
+$(document).ready(function() 
+{	
+	$("body").css("display", "none");
+
+    $("body").fadeIn(2000);
+
+	$("a.transition").click(function(event){
+		event.preventDefault();
+		linkLocation = this.href;
+		$("body").fadeOut(1000, redirectPage);		
+	});
+		
+	function redirectPage() {
+		window.location = linkLocation;
+	}	
+});

771d7c158055f41a3bbef9c6c63df2c11370d321
mrndelfino <39888546+mrndelfino@users.noreply.github.com>
2018-11-04 15:18:34 -0300

    Re-documentation

--- animando/js/animando.js
+++ animando/js/animando.js
@@ -1,18 +1,4 @@
 $(document).ready(function() 
 {	
-	$(".container").css("display", "none").fadeIn(1000);
-
-
-
-	// $(".transition-fade").click(function(event)
-	// {
-	// 	event.preventDefault();
-	// 	linkLocation = this.href;
-	// 	$(".container").fadeOut(2000, redirectPage);		
-	// });
-		
-	// function redirectPage() 
-	// {
-	// 	window.location = linkLocation;
-	// }	
+	$(".container").css("display", "none").fadeIn(1000);	
 });

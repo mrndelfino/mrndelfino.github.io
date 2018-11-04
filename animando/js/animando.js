8bb404432a94f41166c955fa1dc9fd462fa0b81d
mrndelfino <39888546+mrndelfino@users.noreply.github.com>
2018-11-04 04:14:24 -0300

    ---

--- animando/js/animando.js
+++ animando/js/animando.js
@@ -1,14 +1,14 @@
 $(document).ready(function() 
 {	
-	$("div .container").css("display", "none");
+	$(".container").css("display", "none");
 
-    $("div .container").fadeIn(1000);
+    $(".container").fadeIn(1000);
 
 	$("a").click(function(event)
 	{
 		event.preventDefault();
 		linkLocation = this.href;
-		$("div .container").fadeOut(1000, redirectPage);		
+		$(".container").fadeOut(1000, redirectPage);		
 	});
 		
 	function redirectPage() 

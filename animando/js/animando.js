136bee5874898051f052a090c0d24ba9e44572fd
mrndelfino <39888546+mrndelfino@users.noreply.github.com>
2018-11-04 04:58:50 -0300

    Added transition to intro and removed from all the pages

--- animando/js/animando.js
+++ animando/js/animando.js
@@ -1,14 +1,14 @@
 $(document).ready(function() 
 {	
-	$(".container").css("display", "none");
+	$(".container").css("display", "none").fadeIn(1000);
 
-    $(".container").fadeIn(1000);
 
-	$("a").click(function(event)
+
+	$(".transition-fade").click(function(event)
 	{
 		event.preventDefault();
 		linkLocation = this.href;
-		$(".container").fadeOut(1000, redirectPage);		
+		$(".container").fadeOut(2000, redirectPage);		
 	});
 		
 	function redirectPage() 

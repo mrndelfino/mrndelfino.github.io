bac4e199341f29d1d699b010f20731c3cbab904f
mrndelfino <39888546+mrndelfino@users.noreply.github.com>
2018-11-21 01:07:22 -0300

    Fix

--- animando/js/animando.js
+++ animando/js/animando.js
@@ -8,27 +8,29 @@
 
 $(function() 
 {	
-	var fadeTime = parseInt($("fade").text());
-	if(fadeTime != null)
-	{
-		$(".fade").css("display", "none")
-				  .fadeIn(fadeTime);
-	}
+	$(".container").css("display", "none")
+				   .fadeIn(1000);
+	// var fadeTime = parseInt($("fade").text());
+	// if(fadeTime != null)
+	// {
+	// 	$(".fade").css("display", "none")
+	// 			  .fadeIn(fadeTime);
+	// }
 	
-	var swipeTime = parseInt($("swipe").text());
-	var swipeNext =	($("next").text())
-	if(swipeTime != null)
-	{
-		$(".swipe-left").css("display", "none")
-						.animate({"margin-left": screen.width+"px"}, swipeTime)
-		window.location.href = swipeNext;
-	}
+	// var swipeTime = parseInt($("swipe").text());
+	// var swipeNext =	($("next").text())
+	// if(swipeTime != null)
+	// {
+	// 	$(".swipe-left").css("display", "none")
+	// 					.animate({"margin-left": screen.width+"px"}, swipeTime)
+	// 	window.location.href = swipeNext;
+	// }
 	
-	var slideTime = parseInt($("slide").text());
-	var slidePause = parseInt($("pause").text());
-	var slideAmount = parseInt($("amount").text());
-	setInterval(function()
-	{
-		$(".slide-left").animate({"margin-left": "-="+slideAmount+"px"},slideTime)
-	}, slidePause);
+	// var slideTime = parseInt($("slide").text());
+	// var slidePause = parseInt($("pause").text());
+	// var slideAmount = parseInt($("amount").text());
+	// setInterval(function()
+	// {
+	// 	$(".slide-left").animate({"margin-left": "-="+slideAmount+"px"},slideTime)
+	// }, slidePause);
 });

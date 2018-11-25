65282a69e695e6f6b92f9f41976ce8bc606ac686
mrndelfino <39888546+mrndelfino@users.noreply.github.com>
2018-11-25 14:06:49 -0300

    ---

--- animando/js/animando.js
+++ animando/js/animando.js
@@ -10,8 +10,4 @@ $(function()
 {	
 	$(".container, .container-fluid, .fade").css("display", "none")
 				   .fadeIn(1000);
-	
-	var swipeNext =	($("next").text())
-	$(".swipe-left").animate({"margin-left": screen.width+"px"}, 1000)
-	window.location.href = swipeNext;
 });
